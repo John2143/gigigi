4ee13a38b6363e10bb3dd8f987929b9bb4764772
John <john@john2143.com>
2017-08-29 12:07:07 -0400

    Added the same functionality as before but now with vue and also better.
    Add functanilty

--- src/client.js
+++ src/client.js
@@ -1,29 +1,216 @@
+let socket = io();
 
-window.socket = io();
+let LEAGUE = "Harbinger";
 
-let app = new Vue({
-    el: "#app",
-    data: {
-        tabButtons: ["Items", "Currency"],
-        currentTab: "Currency",
-    },
+Vue.mixin({
     methods: {
-        t: function(tab){
-            console.log("asdf", tab);
+        //Currency translate
+        cnfa: id => constData.currencyAbbrList[id][1],
+        //Currency translate abbriv.
+        cnfas: id => constData.currencyAbbrList[id][2][0]
+    },
+});
+
+Vue.component("order", {
+    props: ["order"],
+    template: `
+    <div class="row">
+      <hr>
+
+      <div class="col-xs-2">
+          lul
+      </div>
+      <div class="col-xs-5">
+        {{order.ratiosell.toFixed(3)}} &rArr;
+        1 &rArr;
+        {{order.ratiobuy.toFixed(3)}}
+      </div>
+      <div class="col-xs-5">
+        {{order.buyvalue}}{{cnfas(order.buycurrency)}} &rArr;
+        {{order.sellvalue}}{{cnfas(order.sellcurrency)}}
+      </div>
+
+      <br>
+
+      <div class="col-xs-2" @click="t">
+          message
+      </div>
+      <div class="col-xs-5">
+        {{order.ign}}
+      </div>
+      <div class="col-xs-5" v-if="order.stock">
+        {{order.stock}}{{cnfas(order.sellcurrency)}} stock
+      </div>
+    </div>
+    `,
+    methods: {
+        getBuyText(){
+            return `@${this.order.ign} Hi, I would like to buy
+                your ${this.order.sellvalue} ${this.cnfa(this.order.sellcurrency)} for my
+                ${this.order.buyvalue} ${this.cnfa(this.order.buycurrency)} in ${LEAGUE}`;
         },
-        updateCurr: (a, b) => {
-            socket.emit("updatecurr", [a, b]);
-            console.log("updatecurr", [a, b]);
+        t(){
+            console.log(this.getBuyText());
+        }
+    }
+});
+
+Vue.component("sellerList", {
+    props: ["type", "orders"],
+    template: `
+    <div class="col-xs-6 container">
+      {{cnfa(type)}} Sellers
+      <order v-for="order of orderList" :order="order"></order>
+    </div>
+    `,
+    computed: {
+        orderList(){
+            //slice because sort is in-place ((?TODO test this))
+            let orders = this.orders.slice();
+
+            if(this.$parent.showStockOnly){
+                orders = orders.filter(x => Number(x.stock) > 1);
+            }
+
+            orders.sort((a, b) => b.ratiosell - a.ratiosell);
+            return orders.slice(0, 20);
         },
     },
 });
 
+Vue.component("currencyTab", {
+    template: `
+    <div>
+      <button class="btn btn-primary" @click="updateCurr">update currency rates</button>
+      <div class="checkbox">
+        <label><input type="checkbox" v-model="hasAllCurrencies">all curr</label>
+      </div>
+      <div class="checkbox">
+        <label><input type="checkbox" v-model="showStockOnly">show stock only</label>
+      </div>
+      <button-list :buttons="baseCurrencies.map(cnfa)" :change="setBase"></button-list>
+      <button-list :buttons="hasAllCurrencies ? allOtherCurrencies.map(cnfa) : otherCurrencies.map(cnfa)" :change="setOther"></button-list>
+      <div v-if="currency">
+        <seller-list :type="currency.a" :orders="currency.btoa"></seller-list>
+        <seller-list :type="currency.b" :orders="currency.atob"></seller-list>
+      </div>
+    </div>
+    `,
+    data(){
+        return {
+            currentBase: 4,
+            currentOther: 6,
+            baseCurrencies: [4, 6],
+            otherCurrencies: [],
+            allOtherCurrencies: [],
+            currency: null,
+            hasAllCurrencies: false,
+            showStockOnly: false,
+        };
+    },
+    methods: {
+        updateCurr(){
+            let a = this.currentBase;
+            let b = this.currentOther;
+            socket.emit("updatecurr", [a, b]);
+            console.log("updatecurr", [a, b]);
+        },
+        setBase(id, val){
+            this.currentBase = this.baseCurrencies[id];
+        },
+        setOther(id, val){
+            if(this.hasAllCurrencies){
+                this.currentOther = this.allOtherCurrencies[id];
+            }else{
+                this.currentOther = this.otherCurrencies[id];
+            }
+        },
+    },
+    created(){
+        socket.on("currency", dat => {
+            this.currency = dat;
+            console.log("done", dat);
+        });
 
-setBase = i => $scope.currentBase = i;
-setCandy = i => $scope.currentCandy = i;
+        //Calculate the currencies that should be included based on whether
+        //or not have have a 1 in the start of their data in data.js
+        for(let i = 1; i < constData.currencyAbbrList.length; i++){
+            if(constData.currencyAbbrList[i][0] === 1){
+                this.otherCurrencies.push(i);
+            }
+            this.allOtherCurrencies.push(i);
+        }
+    },
+});
 
-$scope.baseCurrencies = [4, 6]; //chaos and exalt
-$scope.candy = [];
-for(let i = 0; i < constData.currencyAbbrList.length; i++){
-    if(constData.currencyAbbrList[i][0] === 1) $scope.candy.push(i);
-}
+Vue.component("itemsTab", {
+    template: `
+      <div>
+        Todo: items
+      </div>
+    `,
+    data(){
+        return {
+            sounds: {},
+            soundNames: ["whoop.mp3", "alert.mp3"],
+            isMuted: false,
+            alerts: [],
+        };
+    },
+    methods: {
+        playSound(sound){
+            if(sound && this.sounds[sound]) this.sounds[sound].play();
+        },
+        addAlert(dat){
+            if(dat.sound && !this.isMuted) this.playSound(dat.sound);
+            dat.time = new Date();
+            alerts.push(dat);
+        }
+    },
+    created(){
+        //Download required sounds
+        for(let file of this.soundNames){
+            this.sounds[file] = new Audio("/" + file);
+            this.sounds[file].volume = .1;
+        }
+
+        socket.on("alert", (...r) => this.addAlert(...r));
+    }
+});
+
+Vue.component("buttonList", {
+    props: ["buttons", "change"],
+    template: `
+    <div>
+      <button class="btn btn-info"
+        v-for="(val, id) of buttons"
+        @click="setSelected(id, val)"
+        :class="{active: id == currentlySelected}">
+        {{val}}
+      </button>
+    </div>
+    `,
+    data(){
+        return {
+            currentlySelected: null,
+        };
+    },
+    methods: {
+        setSelected(id, val){
+            this.currentlySelected = id;
+            this.change(id, val);
+        },
+    },
+});
+
+let app = new Vue({
+    el: "#app",
+    data: {
+        currentTab: null,
+    },
+    methods: {
+        setCurrentTab(id, val){
+            this.currentTab = val;
+        }
+    },
+});
