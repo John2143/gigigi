dff1c1ca28fa79c12f60330e6d0a67ab6bc44229
John <john@john2143.com>
2017-09-11 05:15:18 -0400

    Fix line ends, prune packages, touch up gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -5,14 +5,10 @@ let browserify = require("browserify");
 let fs = require("fs");
 
 let babel = require("gulp-babel");
-let babelConfig = babel({
-    "plugins": ["transform-es2015-modules-commonjs", "transform-decorators-legacy"]
-});
-
 let sourcemaps = require("gulp-sourcemaps");
 let source = require("vinyl-source-stream");
 let buffer = require("vinyl-buffer");
-let eslint = require("gulp-eslint");
+let changed = require("gulp-changed");
 
 let isProduction = !!gutil.env.prod;
 let watch = !!gutil.env.watch;
@@ -22,7 +18,14 @@ gulp.task("client", ["vue", "css"]);
 gulp.task("vue", () => {
     let src = "./src/client/index.js"
     let br = browserify(src, {debug: !isProduction})
-        .transform("babelify", {presets: ["es2015"], sourceMaps: !isProduction})
+        .transform("babelify", {presets: [
+            ["env", {
+                targets: {
+                    browsers: isProduction ? ["last 3 versions", "IE 11"] : ["last 1 version"]
+                },
+                useBuiltIns: true
+            }],
+        ], sourceMaps: isProduction ? undefined : true})
         .transform("vueify");
 
     if(isProduction){
@@ -32,51 +35,57 @@ gulp.task("vue", () => {
             .transform("uglifyify", {global: true});
     }
 
-    br = br.bundle();
+    br = br.bundle()
+        .pipe(source("index.js"));
 
     if(!isProduction){
-        br = br.pipe(source("index.js"))
-            .pipe(buffer())
+        br.pipe(buffer())
             .pipe(sourcemaps.init({loadMaps: true}))
             .pipe(sourcemaps.write("./"));
     }
 
     return br
-        .pipe(gulp.dest("./client"));
+        .pipe(gulp.dest("client"));
 });
 
 gulp.task("css", () => {
     return gulp.src("src/client/**/*.css")
+        .pipe(changed("client"))
         .pipe(concat("index.css"))
         .pipe(gulp.dest("client"));
 });
 
 gulp.task("server", () => {
     return gulp.src(["src/shared/**/*.js", "src/server/**/*.js"])
-        .pipe(babelConfig)
+        .pipe(changed("server"))
+        .pipe(babel({
+            presets: [
+                ["env", {
+                    targets: {
+                        node: "current"
+                    },
+                    useBuiltIns: true
+                }],
+            ],
+            plugins: [
+                "transform-decorators-legacy",
+            ]
+        }))
         .pipe(gulp.dest("server"));
 });
 
-gulp.task("watchclient", ["watchshared"], () => {
+gulp.task("watchclient", () => {
     gulp.watch("./src/client/**/*.vue", ["vue"]);
     gulp.watch("./src/client/**/*.js", ["vue"]);
     gulp.watch("./src/client/**/*.css", ["css"]);
+    gulp.watch("./src/shared/**/*.js", ["vue"]);
 });
 
-gulp.task("watchshared", () => {
-    gulp.watch("./src/shared/**/*.js", ["server", "vue"]);
-});
-
-gulp.task("watchserver", ["watchshared"], () => {
+gulp.task("watchserver", () => {
     gulp.watch("./src/server/**/*.js", ["server"]);
+    gulp.watch("./src/shared/**/*.js", ["server"]);
 });
 
 gulp.task("default", ["server", "client"]);
 
 gulp.task("watch", ["watchserver", "watchclient"]);
-
-gulp.task("lint", () => {
-    return gulp.src("src/**/*.js")
-        .pipe(eslint())
-        .pipe(eslint.format());
-})
